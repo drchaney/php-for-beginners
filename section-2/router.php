7a31214866b89adbb2e839d480323755a81e8043
David Chaney <david.ray.chaney+github@gmail.com>
2023-09-08 14:44:39 -0500

    refactor router

--- section-2/router.php
+++ section-2/router.php
@@ -1,23 +1,8 @@
 <?php
 
+$routes = require('routes.php');
+
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-// dd($uri);
-
-// if ($uri === '/'){
-//   require 'controllers/index.php';
-// } else if ($uri === '/about'){
-//   require 'controllers/about.php';
-// } else if ($uri === '/contact'){
-//   require 'controllers/contact.php';
-// }
-
-$routes = [ 
-  '/' => 'controllers/index.php',
-  '/about' => 'controllers/about.php',
-  '/notes' => 'controllers/notes.php',
-  '/note' => 'controllers/note.php',
-  '/contact' => 'controllers/contact.php'
-];
 
 function abort($code = 404) {
   http_response_code($code);
